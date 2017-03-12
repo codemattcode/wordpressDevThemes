9ce890eb475718d180e09d841d49b856d850187b
codemattcode <codemattcode@gmail.com>
2017-03-12 04:01:46 +0000

    add margin top to push down posts content

--- blankTheme/content.php
+++ blankTheme/content.php
@@ -11,7 +11,7 @@
       <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
      
 </div>
-<div>    
+<div style="margin-top: 300px;">    
 <!-- add post excerpt/content -->
     <?php echo get_the_excerpt(); ?>
       <a href="<?php the_permalink(); ?>">Read More &raquo;</a>
